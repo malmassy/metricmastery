9449492fcee28293f76e89590d906b76406bbfcd
Mark Almassy <malmassy@seas.upenn.edu>
2024-11-21 08:07:51 -0500

    estimation calcs

--- estimation.js
+++ estimation.js
@@ -2,6 +2,18 @@
       { text: "How long is a pencil?", answer: 18, unit: "cm" },
       { text: "How tall is a giraffe?", answer: 500, unit: "cm" },
       { text: "What is the average weight of a cat?", answer: 4, unit: "kg" },
+      { text: "How <b>long</b> is an unsharpened #2 pencil?", answer: 19, unit: "cm"},
+      { text: "How <b>wide</b> is an unsharpened #2 pencil?", answer: 10, unit: "mm"},
+      { text: "How much does a beach ball weigh?", answer: 0.5, unit: "kg"},
+      { text: "What is the diameter of a beach ball?", answer: 30, unit: "cm"},
+      { text: "What is the volume of a beach ball?", answer: 14_000, unit: "cm<sup>3</sup>"},
+      { text: "What is the circumference of a beach ball?", answer: 94, unit: "cm"},
+      { text: "If the radius of a bowling ball is 10.8cm, what is the circumference?", answer: 68, unit: "cm"},
+      { text: "How tall is the state of Colorado?", answer: 450, unit: "km"},
+      { text: "How wide is the state of Colorado?", answer: 600, unit: "km"},
+      { text: "What is the diameter of a dime?", answer: 18, unit: "mm"},
+      { text: "What is the diameter of a quarter?", answer: 24, unit: "mm"},
+      { text: "What is volume of a dime?", answer: 318, unit: "mm<sup>3</sup>"},                                                  
     ];
 
     const questionElem = document.getElementById("question");
@@ -18,12 +30,12 @@
 
     function loadNewQuestion() {
       currentQuestion = questions[Math.floor(Math.random() * questions.length)];
-      questionElem.textContent = `${currentQuestion.text} (${currentQuestion.unit})`;
+      questionElem.innerHTML = `${currentQuestion.text} (${currentQuestion.unit})`;
       answerInput.value = "";
       resultElem.textContent = "";
       correctIndicator.style.left = "50%";
       userIndicator.style.left = "-10px"; // Reset the user indicator
-      correctLabel.textContent = `Correct Answer: ${currentQuestion.answer}${currentQuestion.unit}`;
+      correctLabel.innerHTML = `Correct Answer: ${currentQuestion.answer}${currentQuestion.unit}`;
     }
 
     function handleSubmit() {
@@ -43,7 +55,7 @@
       const userPosition = correctPosition + (barWidth / 40) * percentageError;
 
       userIndicator.style.left = `${Math.min(Math.max(userPosition, 0), barWidth - 2)}px`;
-      userLabel.textContent = `Your Answer: ${userAnswer}${currentQuestion.unit}`;
+      userLabel.innerHTML = `Your Answer: ${userAnswer}${currentQuestion.unit}`;
 
       // Feedback for the result
       let points = 0;
