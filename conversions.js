5f83d5bce33f9e780372c986f20757fc06438c59
Mark Almassy <malmassy@seas.upenn.edu>
2025-01-19 20:00:13 -0500

    Adding complex unit conversions.

--- conversions.js
+++ conversions.js
@@ -10,9 +10,16 @@ const units = {
     liters: { symbol: 'L' },
 };
 
+const unitExponents = {
+    base: { value: 1, fullText: '', expText: ''},
+    square: { value: 2, fullText: 'square ', expText: '<sup>2</sup>'},
+    cubic: { value: 3, fullText: 'cubic ', expText: '<sup>3</sup>'}
+}
+
 // Variables to store problem details
-let quesM, quesN, inputPrefix, outputPrefix, unitExp;
-let uExpVal, iPrefixVal, oPrefixVal, ansN;
+let quesM, quesN, numeratorUnits, numInputPrefix, numOutputPrefix, numUnitExp;
+let hasDenominatorUnits = false, denInputPrefix = 'base', denOutputPrefix = 'base', denUnitExp = 'base';
+let useScientificNotation, useFullUnitName;
 
 // Utility Functions
 function getRandomNumber(min, max, decimals) {
@@ -25,12 +32,6 @@ function getRandomElement(array) {
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getValueForQuestion() {
-    quesM = getRandomNumber(1, 10, 2);
-    quesN = getRandomInteger(-7, 10);
-    return [quesM, quesN];
 }
 
 function getValueFromSN(m, n) {
@@ -59,87 +60,188 @@ function clearAnswerAndFeedback() {
 
 }
 
+function setProblemValues() {
+    numeratorUnits = getRandomElement(Object.keys(units));
+
+    quesM = getRandomNumber(1, 10, 2);
+    quesN = getRandomInteger(-7, 10);
+    numInputPrefix = getRandomElement(Object.keys(metricPrefixes));
+
+    do {
+        numOutputPrefix = getRandomElement(Object.keys(metricPrefixes));
+    } while (numOutputPrefix === numInputPrefix || numOutputPrefix == 'micro');
+
+    if (numeratorUnits == 'meters') {
+        numUnitExp = getRandomElement(Object.keys(unitExponents));
+        hasDenominatorUnits = false;
+    } else {
+        numUnitExp = 'base';
+        hasDenominatorUnits = Math.random() < 0.4;
+    }
+
+    if (hasDenominatorUnits) {
+        denInputPrefix = getRandomElement(Object.keys(metricPrefixes));
+
+        do {
+            denOutputPrefix = getRandomElement(Object.keys(metricPrefixes));
+        } while (denInputPrefix === denOutputPrefix || denOutputPrefix == 'micro');
+
+        denUnitExp = getRandomElement(Object.keys(unitExponents));
+    } else {
+        denInputPrefix = 'base';
+        denOutputPrefix = 'base';
+        denUnitExp = 'base';
+    }
+}
+
+function getAnswerN(showAnswer) {
+
+    if (showAnswer) {
+        console.log(
+            `${quesN} + ${unitExponents[numUnitExp].value} * (${metricPrefixes[numInputPrefix].exponent} - ${metricPrefixes[numOutputPrefix].exponent}) -((${metricPrefixes[denInputPrefix].exponent} - ${metricPrefixes[denOutputPrefix].exponent}) * ${unitExponents[denUnitExp].value});`
+        );
+    }
+
+
+    return quesN + 
+        unitExponents[numUnitExp].value * 
+        (metricPrefixes[numInputPrefix].exponent - metricPrefixes[numOutputPrefix].exponent) -
+        ((metricPrefixes[denInputPrefix].exponent - metricPrefixes[denOutputPrefix].exponent) * unitExponents[denUnitExp].value);
+}
+
+function getTextFullUnitsNoDenom(inputOrOutput) {
+    // e.g. 'cubic millimeters'
+    if (inputOrOutput == 'input') {
+        return `${unitExponents[numUnitExp].fullText}${numInputPrefix == 'base' ? '' : numInputPrefix}${numeratorUnits}`;
+    } else {
+        return `${unitExponents[numUnitExp].fullText}${numOutputPrefix == 'base' ? '' : numOutputPrefix}${numeratorUnits}`;
+    }
+}
+
+function getTextAbbrevUnitsNoDenom(inputOrOutput) {
+    if (inputOrOutput == 'input') {
+        return `${metricPrefixes[numInputPrefix].symbol}${units[numeratorUnits].symbol}${unitExponents[numUnitExp].expText}`;
+    } else {
+        return `${metricPrefixes[numOutputPrefix].symbol}${units[numeratorUnits].symbol}${unitExponents[numUnitExp].expText}`;
+    }
+}
+
+function getTextFullUnitsWithDenom(inputOrOutput) {
+    if (inputOrOutput == 'input') {
+        return `${numInputPrefix == 'base' ? '' : numInputPrefix}${numeratorUnits} 
+        per ${unitExponents[denUnitExp].fullText}${denInputPrefix == 'base' ? '' : denInputPrefix}meter`;
+    } else {
+        return `${numOutputPrefix == 'base' ? '' : numOutputPrefix}${numeratorUnits} 
+        per ${unitExponents[denUnitExp].fullText}${denOutputPrefix == 'base' ? '' : denOutputPrefix}meter`;
+    }
+}
+
+function getTextAbbrevUnitsWithDenom(inputOrOutput) {
+        if (inputOrOutput == 'input') {
+        return `${metricPrefixes[numInputPrefix].symbol}${units[numeratorUnits].symbol}${unitExponents[numUnitExp].expText}/${metricPrefixes[denInputPrefix].symbol}m${unitExponents[denUnitExp].expText}`;
+    } else {
+        return `${metricPrefixes[numOutputPrefix].symbol}${units[numeratorUnits].symbol}${unitExponents[numUnitExp].expText}/${metricPrefixes[denOutputPrefix].symbol}m${unitExponents[denUnitExp].expText}`;        
+    }
+}
+
+function setQuestionText() {
+    useScientificNotation = Math.random() < 0.5;
+    useFullUnitName = Math.random() < 0.5;
+
+    let mDisplayText;
+
+    if (useScientificNotation) {
+        mDisplayText = `${quesM} x 10<sup>${quesN}</sup>`;
+    } else {
+        mDisplayText = `${getValueFromSN(quesM, quesN)}`;
+    }
+
+    let iDisplay, oDisplay;
+
+    if (hasDenominatorUnits && useFullUnitName) {
+        iDisplay = getTextFullUnitsWithDenom('input');
+        oDisplay = getTextFullUnitsWithDenom('output');
+    } else if (!hasDenominatorUnits && useFullUnitName) {
+        iDisplay = getTextFullUnitsNoDenom('input');
+        oDisplay = getTextFullUnitsNoDenom('output');
+    } else if (hasDenominatorUnits && !useFullUnitName) {
+        iDisplay = getTextAbbrevUnitsWithDenom('input');
+        oDisplay = getTextAbbrevUnitsWithDenom('output');
+    } else {
+        iDisplay = getTextAbbrevUnitsNoDenom('input');
+        oDisplay = getTextAbbrevUnitsNoDenom('output');
+    }
+
+    document.getElementById('problem').innerHTML = `Convert ${mDisplayText} ${iDisplay} to ${oDisplay}.`;
+    document.getElementById('unitDisplay').innerHTML = `${oDisplay}`;
+}
+
 // Main Function to Generate Problems
 function generateProblem() {
     collapseAll();
     clearAnswerAndFeedback();
 
-    const unit = getRandomElement(Object.keys(units));
-    let unitExp = 1;
-
-    if (unit == 'meters') {
-        unitExp = getRandomInteger(1, 3);
-    }
-
-    // Generate prefixes for input and output
-    let inputPrefix, outputPrefix;
+    // Avoid problematic cases where the answer is too large.    
     do {
-        inputPrefix = getRandomElement(Object.keys(metricPrefixes));
-        outputPrefix = getRandomElement(Object.keys(metricPrefixes));
-    } while (outputPrefix === inputPrefix || outputPrefix == 'micro');
+        setProblemValues();
+    } while (Math.abs(getAnswerN() > 47));
 
-    // Generate question values
-    const quesM = getRandomNumber(1, 10, 2);
-    const quesN = getRandomInteger(-7, 10);
+    console.log(getAnswerN(true));
 
-    // Calculate the answer
-    const inputExp = metricPrefixes[inputPrefix].exponent;
-    const outputExp = metricPrefixes[outputPrefix].exponent;
-    const ansN = quesN + unitExp * (inputExp - outputExp);
+    setQuestionText();
 
-    // Avoid problematic cases
-    if (Math.abs(ansN) > 47) {
-        return generateProblem();
+    // Save problem details for validation
+    window.problemDetails = { quesM, quesN, numeratorUnits, numInputPrefix, numOutputPrefix, numUnitExp,
+        denInputPrefix, denOutputPrefix, denUnitExp
+    };
+
+    if (hasDenominatorUnits) {
+        document.getElementById('stepZero').style.display = 'inline-block';
+        updateStepZeroHint();
+    } else {
+        document.getElementById('stepZero').style.display = 'none';
+    }
+    updateStepOneHint();
+    updateStepTwoHint();
+    updateStepThreeHint();
+    updateStepFourHint();
+
+    if (hasDenominatorUnits) {
+        document.getElementById('stepThreeB').style.display = 'inline-block';
+        updateStepThreeBHint();
+    } else {
+        document.getElementById('stepThreeB').style.display = 'none';
     }
 
-    /*
-    console.log(quesM);
-    console.log(quesN);
-    console.log(ansN);
-    */
+}
 
-    // Format the question
-    const useScientificNotation = Math.random() < 0.5;
-    const useFullUnitName = Math.random() < 0.5;
-    const quesDisplay = useScientificNotation
-        ? `${quesM} x 10<sup>${quesN}</sup>`
-        : `${getValueFromSN(quesM, quesN)}`;
+function updateStepZeroHint() {
+    const stepZeroHint = document.getElementById('step-0-hint');
 
-    let iDisplay;
-    let oDisplay;
+    let iDisplay, oDisplay;
 
-    let unitExpText;
+    let mDisplayText;
 
-    if (unitExp == 2) {
-        unitExpText = "square ";
-    } else if (unitExp == 3) {
-        unitExpText = "cubic ";
+    if (useScientificNotation) {
+        mDisplayText = `${quesM} x 10<sup>${quesN}</sup>`;
     } else {
-        unitExpText = "";
+        mDisplayText = `${getValueFromSN(quesM, quesN)}`;
     }
 
     if (useFullUnitName) {
-        iDisplay = `${unitExpText}${inputPrefix === 'base' ? '' : inputPrefix}${unit}`;
-        oDisplay = `${unitExpText}${outputPrefix === 'base' ? '' : outputPrefix}${unit}`;
+        iDisplay = getTextFullUnitsNoDenom('input');
+        oDisplay = getTextFullUnitsNoDenom('output');
     } else {
-        iDisplay = `${metricPrefixes[inputPrefix].symbol}${units[unit].symbol}<sup>${unitExp == 1 ? '' : unitExp}</sup>`;
-        oDisplay = `${metricPrefixes[outputPrefix].symbol}${units[unit].symbol}<sup>${unitExp == 1 ? '' : unitExp}</sup>`;
+        iDisplay = getTextAbbrevUnitsNoDenom('input');
+        oDisplay = getTextAbbrevUnitsNoDenom('output');
     }
 
-    // Display the problem
-    const problemElement = document.getElementById('problem');
-    problemElement.innerHTML = `Convert ${quesDisplay} ${iDisplay} to ${oDisplay}.`;
+    let numProblem = `Convert ${mDisplayText} ${iDisplay} to ${oDisplay}.`;
 
-    // Save problem details for validation
-    window.problemDetails = { quesM, quesN, ansN, unit, inputPrefix, outputPrefix, unitExp };
-
-    updateStepOneHint(useScientificNotation, quesM, quesN);
-    updateStepTwoHint(inputPrefix, outputPrefix);
-    updateStepThreeHint(inputExp, outputExp, unitExp, quesN, ansN);
-    updateStepFourHint(quesM, ansN, oDisplay);
+    stepZeroHint.innerHTML = `Ignore the denominator units for now, and first solve as if the problem is just:</br><b>${numProblem}</b>`;
 }
 
-function updateStepOneHint(useScientificNotation, quesM, quesN) {
+function updateStepOneHint() {
     const stepOneHint = document.getElementById('step-1-hint');
     let hint;
 
@@ -152,84 +254,86 @@ function updateStepOneHint(useScientificNotation, quesM, quesN) {
     stepOneHint.innerHTML = hint;
 }
 
-function updateStepTwoHint(inputPrefix, outputPrefix) {
+function updateStepTwoHint() {
     const stepTwoHint = document.getElementById('step-2-hint');
     let hint;
 
-    hint = `${inputPrefix}, ${metricPrefixes[inputPrefix].symbol}, <b>${metricPrefixes[inputPrefix].exponent}</b>
+    hint = `<b>Inputs:</b> ${numInputPrefix}, ${metricPrefixes[numInputPrefix].symbol}, <b>${metricPrefixes[numInputPrefix].exponent}</b>
             </br>
-            ${outputPrefix}, ${metricPrefixes[outputPrefix].symbol}, <b>${metricPrefixes[outputPrefix].exponent}</b>`;
+            <b>Outputs:</b> ${numOutputPrefix}, ${metricPrefixes[numOutputPrefix].symbol}, <b>${metricPrefixes[numOutputPrefix].exponent}</b>`;
 
     stepTwoHint.innerHTML = hint;
 }
 
-function updateStepThreeHint(inputExp, outputExp, unitExp, quesN, ansN) {
+function updateStepThreeHint() {
     const stepThreeHint = document.getElementById('step-3-hint');
+
+    let finalValue = (metricPrefixes[numInputPrefix].exponent - metricPrefixes[numOutputPrefix].exponent) * unitExponents[numUnitExp].value + quesN;
+
     let hint = `<p>
-        "I" Input Prefix Exponent: ${inputExp}</br>
-        "O" Output Prefix Exponent: ${outputExp}</br>
-        "U" Unit Exponent: ${unitExp}</br>
+        "I" Input Prefix Exponent: ${metricPrefixes[numInputPrefix].exponent}</br>
+        "O" Output Prefix Exponent: ${metricPrefixes[numOutputPrefix].exponent}</br>
+        "U" Unit Exponent: ${numUnitExp}</br>
         "Q" Question Value Exponent:  ${quesN}</br></br>
         (I - O) x U + Q &rarr;
-        [ (<u>${inputExp}</u> - <u>${outputExp}</u>) x <u>${unitExp}</u> + <u>${quesN}</u> ] = <b>${ansN}</b>.
+        [ (<u>${metricPrefixes[numInputPrefix].exponent}</u> - <u>${metricPrefixes[numOutputPrefix].exponent}</u>) x <u>${unitExponents[numUnitExp].value}</u> + <u>${quesN}</u> ] = <b>${finalValue}</b>.
     </p>`;
 
     stepThreeHint.innerHTML = hint;
 }
 
-function updateStepFourHint(quesM, ansN, oDisplay) {
+
+function updateStepThreeBHint() {
+    const stepThreeBHint = document.getElementById('step-3b-hint');
+
+    let hintText = 'Convert from the input unit of the denominator to the output, invert it (flip the sign of the exponent), and then add it to your exponent answer from step 3.'
+
+
+    stepThreeBHint.innerHTML = hintText;
+}
+
+function updateStepFourHint() {
     const stepFourHint = document.getElementById('step-4-hint');
-    let hint = `${quesM} x 10<sup>${ansN}</sup> ${oDisplay}.`;
+    let hint = `${quesM} x 10<sup>${getAnswerN()}</sup> ${document.getElementById('unitDisplay').innerHTML}.`;
 
     stepFourHint.innerHTML = hint;
 }
 
-function checkAnswer() {
-    document.getElementById('feedback').style.display = 'inline-block';
 
-    const answer = document.getElementById('answerInput').value.trim();
+function checkAnswer() {
     const feedbackElement = document.getElementById('feedback');
 
-    const regex = /^([0-9]\.[\d]+)\s?(?:[xX]\s?10)\s?\^(-?\d+)\s?([a-zA-Z]+)(?:\^([1-9]))?(?:[\s\.]*)$/;
-    const match = answer.match(regex);
+    feedbackElement.style.display = 'inline-block';
 
-    let ansM, base, ansN, ansUnits, ansUnitsExp;
+    const answer = document.getElementById('answerInput').value.trim();
+
+    // Parse out the user's answer:
+    const regex = /^([0-9]\.[\d]+)\s?(?:[xX]\s?10)\s?\^(-?\d+)$/;
+    const match = answer.match(regex);
 
     if (!match) {
         feedbackElement.style.textAlign = "left";
         feedbackElement.innerHTML = `
             Your answer does not appear to be properly formatted.</br></br>
-            It should be something like:</br>1.23 x 10^5 cm^2</br>1.23x10^4 deciliters.
-            `;
+            It should be something like: 1.23 x 10^5`;
 
         return;
     }
 
+    let ansM, base, ansN, ansUnits, ansUnitsExp;
+
     ansM = match[1];
     ansN = match[2];
-    ansUnits = match[3];
-    ansUnitsExp = (match[4] == null ? 1 : match[4]);
-    console.log(`ansM: ${ansM}\nansN: ${ansN}\nansUnits: ${ansUnits}\nansUnitsExp: ${ansUnitsExp}`);
-    console.log(`corrM: ${window.problemDetails.quesM}\ncorrN: ${window.problemDetails.ansN}\ncorrUnitExp: ${window.problemDetails.unitExp}`);
-    
-    let acceptableUnits = [
-        `${window.problemDetails.outputPrefix === 'base' ? '' : window.problemDetails.outputPrefix}${window.problemDetails.unit}`,
-        `${metricPrefixes[window.problemDetails.outputPrefix].symbol}${units[window.problemDetails.unit].symbol}`
-        ];
-
+    console.log(`userM: ${ansM}\nuserN: ${ansN}`);
+    console.log(`corrM: ${window.problemDetails.quesM}\ncorrN: ${getAnswerN()}`);
 
     mCorrect = ansM == window.problemDetails.quesM;
-    nCorrect = ansN == window.problemDetails.ansN;
-    unitsCorrect = acceptableUnits.includes(ansUnits);
-    unitExpCorrect = ansUnitsExp == window.problemDetails.unitExp;
-
+    nCorrect = ansN == getAnswerN();
 
     // Generate feedback for each component
     const feedbackItems = [
         { correct: mCorrect, label: 'Mantissa' },
         { correct: nCorrect, label: 'Exponent' },
-        { correct: unitsCorrect, label: 'Units' },
-        { correct: unitExpCorrect, label: 'Unit Exponent' },
     ];
 
     feedbackElement.innerHTML = feedbackItems.map(item => `
